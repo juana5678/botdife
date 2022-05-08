447a0f535d25fd3a8baa4c593e98e43795e6a894
Ruben323h <99520739+Ruben323h@users.noreply.github.com>
2022-05-08 17:37:29 -0500

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,5 @@
+api_hash = "f7b699c7cd39b54c5b01ba97ed7d1c64"
+api_id = 1736372819
+bot_token = "5314689333:AAFT4uCncjBV6j1F939v8nIYIG1IiOatzn0"
+authorized_users = []
+admin_user = 'Leosmelitho720'
