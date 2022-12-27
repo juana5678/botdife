c6507019897fa57543faaf83704c45c89fbd1411
juana5678 <118631590+juana5678@users.noreply.github.com>
2022-12-27 13:13:31 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "f7b699c7cd39b54c5b01ba97ed7d1c64"
 api_id = 1736372819
-bot_token = "5965546694:AAE-VfOcsFkuCe_CA7A3rpmYh2OQFleDYDk'
+bot_token ="5965546694:AAE-VfOcsFkuCe_CA7A3rpmYh2OQFleDYDk'
 authorized_users = []
 admin_user = 'JAGB2021'
